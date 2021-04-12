c148b563cb5ca5c7c4054a90006d834d94ddbdc8
wangbinqiang <wangbinqiang@inspur.com>
2021-04-12 12:14:44 -0400

    two try

--- imix/models/losses/__init__.py
+++ imix/models/losses/__init__.py
@@ -1,9 +1,10 @@
 from .diverse_loss import DiverseLoss
 from .triple_logit_binary_cross_entropy import CrossEntropyLoss, OBJCrossEntropyLoss, TripleLogitBinaryCrossEntropy, BCEWithLogitsLoss
 from .yolo_loss import YOLOLoss
-from .yolo_loss_v2 import YOLOLossV2
+from .yolo_loss_v2 import YOLOLossV
 
 __all__ = [
-    'TripleLogitBinaryCrossEntropy', 'YOLOLoss', 'YOLOLossV2', 'DiverseLoss', 'CrossEntropyLoss', 'OBJCrossEntropyLoss',
+    'TripleLogitBinaryCrossEntropy', 'YOLOLoss',
+    'YOLOLossV2', 'DiverseLoss', 'CrossEntropyLoss', 'OBJCrossEntropyLoss',
     'BCEWithLogitsLoss'
 ]
