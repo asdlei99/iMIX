ad2897a8dbe0058f2708c0033796762636c29d05
jinliang106 <jinliang106>
2021-04-25 11:30:44 -0400

    Fixed a bug which in calculating the number for iterations(max_iter) when is_lr_accumulation is True and gradient_acculation_steps >1 . 	modified:   imix/engine/organizer.py

--- imix/engine/organizer.py
+++ imix/engine/organizer.py
@@ -85,6 +85,9 @@ class Organizer:
         self.start_epoch = 0
         self.start_iter = 0
         self.max_iter = cfg.total_epochs * len(self.train_data_loader) if self.by_epoch else cfg.max_iter
+        if self.is_lr_accumulation and self.gradient_accumulation_steps > 1:
+            self.max_iter /= self.gradient_accumulation_steps
+
         self.max_epoch = cfg.total_epochs if self.by_epoch else 0
 
         self.hooks = self.build_hooks()
