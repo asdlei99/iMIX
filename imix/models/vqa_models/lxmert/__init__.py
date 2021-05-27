925ae62da52a892f56127e3f0a42afac75012069
zj <zhao_jian@inspur.com>
2021-05-27 17:21:15 -0400

    1.remove unused lib in requirement 2.remove unused code

--- imix/models/vqa_models/lxmert/__init__.py
+++ imix/models/vqa_models/lxmert/__init__.py
@@ -3,10 +3,13 @@ from .lxmert_task import LXMERT
 from .datasets.lxmert_vqa import VQATorchDataset
 from .datasets.lxmert_gqa import GQATorchDataset
 from .datasets.lxmert_nlvr2 import NLVR2TorchDataset
-from .datasets.lxmertpretrain_loader import LXMERTPreTrainDATASET
 from .postprocess_evaluator import LXMERT_VQAAccuracyMetric
 
 __all__ = [
-    'LXMERT', 'LXMERTForPretraining', 'VQATorchDataset', 'GQATorchDataset', 'NLVR2TorchDataset',
-    'LXMERTPreTrainDATASET', 'LXMERT_VQAAccuracyMetric'
+    'LXMERT',
+    'LXMERTForPretraining',
+    'VQATorchDataset',
+    'GQATorchDataset',
+    'NLVR2TorchDataset',
+    'LXMERT_VQAAccuracyMetric',
 ]
--- imix/models/vqa_models/lxmert/datasets/__init__.py
+++ imix/models/vqa_models/lxmert/datasets/__init__.py
@@ -1,6 +1,9 @@
 from .lxmert_vqa import VQATorchDataset
 from .lxmert_gqa import GQATorchDataset
 from .lxmert_nlvr2 import NLVR2TorchDataset
-from .lxmertpretrain_loader import LXMERTPreTrainDATASET
 
-__all__ = ['VQATorchDataset', 'GQATorchDataset', 'NLVR2TorchDataset', 'LXMERTPreTrainDATASET']
+__all__ = [
+    'VQATorchDataset',
+    'GQATorchDataset',
+    'NLVR2TorchDataset',
+]
