f1c03c8d710fd291a40f1d8cdadbaf962afada0a
jinliang106 <jinliang106>
2021-06-05 13:30:19 -0400

    modify get the version code and fix fp16 bug

--- imix/version.py
+++ imix/version.py
@@ -1,19 +1,12 @@
-# Copyright (c) Open-MMLab. All rights reserved.
+# Copyright (c) Inspur, Inc. and its affiliates. All Rights Reserved
 
-__version__ = '0.0.1'
+__version__ = '0.1'
 short_version = __version__
 
 
 def parse_version_info(version_str):
-    version_info = []
-    for x in version_str.split('.'):
-        if x.isdigit():
-            version_info.append(int(x))
-        elif x.find('rc') != -1:
-            patch_version = x.split('rc')
-            version_info.append(int(patch_version[0]))
-            version_info.append(f'rc{patch_version[1]}')
-    return tuple(version_info)
+    ver_info = [d for d in version_str.split('.') if d.isdigit()]
+    return tuple(ver_info)
 
 
 version_info = parse_version_info(__version__)
